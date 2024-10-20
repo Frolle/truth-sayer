33d96d8e20dd9bf2b5134ca60be0668b338ddaa4
Simon Dirnberger <simon.dirnberger@gu.se>
2024-10-20 15:51:25 +0200

    * Upgrade to Angular 18.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TruthRandomizerComponent } from './truth-randomizer/truth-randomizer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TruthRandomizerComponent} from './truth-randomizer/truth-randomizer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHttpClient} from '@angular/common/http';
+import {MatButton} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -17,9 +19,12 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButton,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/truth-randomizer/truth-randomizer.component.spec.ts
+++ src/app/truth-randomizer/truth-randomizer.component.spec.ts
@@ -1,12 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { TruthRandomizerComponent } from './truth-randomizer.component';
+import {TruthRandomizerComponent} from './truth-randomizer.component';
 
 describe('TruthRandomizerComponent', () => {
   let component: TruthRandomizerComponent;
   let fixture: ComponentFixture<TruthRandomizerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TruthRandomizerComponent ]
     })
